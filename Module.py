8366721c5cbe5d9347f91f01ad5d3d3305b4042f
S. M. Asif Mahfuz <55946060+Asif1405@users.noreply.github.com>
2019-10-10 05:54:10 +0600

    Create Module.py

---
+++ Module.py
@@ -0,0 +1,28 @@
+#creating clasifier
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+
+
+#loading documents
+documents_f = open("documents.pickle", "rb")
+documents = pickle.load(documents_f)
+documents_f.close()
+
